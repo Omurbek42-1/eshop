ea00b3a1949e059b6e86b88d9002599425e40136
omurbek <omurbekzaanbaeva@gmail.com>
2024-08-13 18:20:47 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import DirectorListCreate, DirectorRetrieve, MovieListCreate, MovieRetrieve, ReviewListCreate, ReviewRetrieve
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('movie_app.urls')),
+    path('directors/', DirectorListCreate.as_view(), name='director-list'),
+    path('directors/<int:pk>/', DirectorRetrieve.as_view(), name='director-detail'),
+    path('movies/', MovieListCreate.as_view(), name='movie-list'),
+    path('movies/<int:pk>/', MovieRetrieve.as_view(), name='movie-detail'),
+    path('reviews/', ReviewListCreate.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', ReviewRetrieve.as_view(), name='review-detail'),
 ]
